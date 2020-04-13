17f03c585ba0d884148a55aa28280969313596eb
mgechev <mgechev@gmail.com>
2020-04-13 13:07:51 -0700

    Switch to webpack@5

--- commonjs/index.js
+++ commonjs/index.js
@@ -1,4 +1,3 @@
 import { add } from './utils';
 
 console.log(add(1, 2));
-
--- commonjs/utils.js
+++ commonjs/utils.js
@@ -2,4 +2,6 @@ module.exports.add = (a, b) => a + b;
 module.exports.subtract = (a, b) => a - b;
 module.exports.multiply = (a, b) => a * b;
 module.exports.divide = (a, b) => a / b;
-module.exports.max = arr => require('lodash-es').maxBy(arr);
+
+const { maxBy } = require('lodash-es');
+module.exports.max = arr => maxBy(arr);
